078c1f89d023c9ba465192a35c0dd2de5d675ba1
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-17 22:57:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma (phrase)
 
     if phrase == phrase.upcase
-    puts 'NO, NOT SINCE 1938!'
+     'NO, NOT SINCE 1938!'
     
   elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
