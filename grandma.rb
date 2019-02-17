c38840656b00659bd8f993c39dea94ba087fa996
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-17 22:41:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma (phrase)
+
+    if phrase == phrase.upcase
+    puts 'NO, NOT SINCE 1938!'
+    
+  elsif phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU GRANDMA!"
+    
+    else 
+    puts 'HUH?! SPEAK UP, SONNY!'
+  end
+  
+  # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
